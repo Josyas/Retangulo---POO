a05005236edd6975ad7680d7c242be413515bdf1
Josias <87546094+Josyas@users.noreply.github.com>
2021-08-10 10:39:24 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+
+namespace Ex1ClasseAtributoMetodo
+{
+    class Program
+    {
+        static void Main(string[] args)     
+        {
+            Retangulo tot = new Retangulo();
+
+            Console.WriteLine("Entre a largura e altura do retângulo: ");
+            tot.Largura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            tot.Altura  = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+           
+
+            Console.WriteLine(tot.Area().ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine(tot.Perimetro().ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine(tot.Diagonal().ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
